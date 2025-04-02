8db4f56ca6fcbab3455a6a9a9fb23f612e3d903d
Juan Pablo Alvarado Vallejo <juan.alvarado@idom.com>
2025-04-02 17:43:22 +0200

    feat(Database): :sparkles: Add DatabaseModule for TypeORM configuration

---
+++ src/database/database.module.ts
@@ -0,0 +1,28 @@
+import { Global, Module } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import config from 'src/config';
+
+@Global()
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [config.KEY],
+      useFactory: (configService: ConfigType<typeof config>) => {
+        const { database } = configService;
+        return {
+          type: 'postgres',
+          username: database.user,
+          host: database.host,
+          database: database.dbName,
+          password: database.password,
+          port: database.port,
+          ssl: true,
+          autoLoadEntities: true
+        }
+      },
+    })
+  ],
+  exports: [TypeOrmModule]
+})
+export class DatabaseModule {}
