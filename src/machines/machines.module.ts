16deaa13987e32c011a2f4b84b21c06b2ed84ba9
Juan Pablo Alvarado Vallejo <juan.alvarado@idom.com>
2025-04-03 20:45:15 +0200

    feat(Machines): :recycle: Move locations controller and service to machines module

--- src/machines/machines.module.ts
+++ src/machines/machines.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { MachineStatus } from './entities/machine-status.entity';
+import { Location } from './entities/location.entity';
 import { MachineStatusController } from './controllers/machine-status.controller';
 import { MachineStatusService } from './services/machine-status.service';
+import { LocationsService } from './services/locations.service';
+import { LocationsController } from './controllers/locations.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([MachineStatus])],
-  providers: [MachineStatusService],
-  controllers: [MachineStatusController]
+  imports: [TypeOrmModule.forFeature([MachineStatus, Location])],
+  providers: [MachineStatusService, LocationsService],
+  controllers: [MachineStatusController, LocationsController]
 })
 export class MachinesModule {}
