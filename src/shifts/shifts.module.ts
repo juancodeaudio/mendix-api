4198308a043cc5ed8a83802713844e50e7b19546
Juan Pablo Alvarado Vallejo <juan.alvarado@idom.com>
2025-04-03 18:16:25 +0200

    feat(Shifts): :bug: Add User entity to Shifts module

--- src/shifts/shifts.module.ts
+++ src/shifts/shifts.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Shift } from './entities/shift.entity';
+import { User } from 'src/users/entities/user.entity';
 import { ShiftsController } from './controllers/shifts.controller';
 import { ShiftsService } from './services/shifts.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Shift])],
+  imports: [TypeOrmModule.forFeature([Shift, User])],
   controllers: [ShiftsController],
   providers: [ShiftsService]
 })
