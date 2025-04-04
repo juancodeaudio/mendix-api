f521e36c4276a8331a7e5c4a47051604ec6c6ebb
Juan Pablo Alvarado Vallejo <juan.alvarado@idom.com>
2025-04-04 19:01:17 +0200

    refactor(Machines): :recycle: Refactor MachinesService to use repositories for Location and MachineStatus

--- src/machines/services/machines.service.ts
+++ src/machines/services/machines.service.ts
@@ -2,16 +2,16 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Machine } from '../entities/machine.entity';
-import { MachineStatusService } from './machine-status.service';
-import { LocationsService } from './locations.service';
+import { Location } from '../entities/location.entity';
+import { MachineStatus } from '../entities/machine-status.entity';
 import { CreateMachineDto, UpdateMachineDto } from '../dtos/machines.dto';
 
 @Injectable()
 export class MachinesService {
   constructor(
     @InjectRepository(Machine) private machineRepo: Repository<Machine>,
-    private machineStatusService: MachineStatusService,
-    private locationsService: LocationsService,
+    @InjectRepository(Location) private locationsRepo: Repository<Location>,
+    @InjectRepository(MachineStatus) private machineStatusRepo: Repository<MachineStatus>,
   ) {}
 
   findAll() {
@@ -34,11 +34,17 @@ export class MachinesService {
   async createMachine(data: CreateMachineDto) {
     const newMachine = this.machineRepo.create(data);
     if (data.machineStatusId) {
-      const machineStatus = await this.machineStatusService.findOne(data.machineStatusId);
+      const machineStatus = await this.machineStatusRepo.findOne({ where: { id: data.machineStatusId } });
+      if (!machineStatus) {
+        throw new NotFoundException(`MachineStatus #${data.machineStatusId} not found`);
+      }
       newMachine.machineStatus = machineStatus;
     }
     if (data.locationId) {
-      const location = await this.locationsService.findOne(data.locationId);
+      const location = await this.locationsRepo.findOne({ where: { id: data.locationId } });
+      if (!location) {
+        throw new NotFoundException(`Location #${data.locationId} not found`);
+      }
       newMachine.location = location;
     }
     return this.machineRepo.save(newMachine);
@@ -50,11 +56,17 @@ export class MachinesService {
       throw new NotFoundException(`Machine #${id} not found`);
     }
     if (changes.machineStatusId) {
-      const machineStatus = await this.machineStatusService.findOne(changes.machineStatusId);
+      const machineStatus = await this.machineStatusRepo.findOne({ where: { id: changes.machineStatusId } });
+      if (!machineStatus) {
+        throw new NotFoundException(`MachineStatus #${changes.machineStatusId} not found`);
+      }
       machine.machineStatus = machineStatus;
     }
     if (changes.locationId) {
-      const location = await this.locationsService.findOne(changes.locationId);
+      const location = await this.locationsRepo.findOne({ where: { id: changes.locationId } });
+      if (!location) {
+        throw new NotFoundException(`Location #${changes.locationId} not found`);
+      }
       machine.location = location;
     }
     this.machineRepo.merge(machine, changes);
