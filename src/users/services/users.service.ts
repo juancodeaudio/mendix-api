9321c8c0fb39eb3f77da832fc987aea0f0f02af1
Juan Pablo Alvarado Vallejo <juan.alvarado@idom.com>
2025-04-04 19:02:08 +0200

    refactor(Users): :recycle: Refactor UsersService to use repositories for Role and Shift

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -2,16 +2,16 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../entities/user.entity';
-import { RolesService } from './roles.service';
-import { ShiftsService } from './shifts.service';
+import { Role } from '../entities/role.entity';
+import { Shift } from '../entities/shift.entity';
 import { CreateUserDto, UpdateUserDto } from '../dtos/users.dto';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private userRepo: Repository<User>,
-    private rolesService: RolesService,
-    private shiftsService: ShiftsService,
+    @InjectRepository(Role) private rolesRepo: Repository<Role>,
+    @InjectRepository(Shift) private shiftsRepo: Repository<Shift>,
   ) {}
 
   findAll() {
@@ -34,11 +34,17 @@ export class UsersService {
   async createUser(data: CreateUserDto) {
     const newUser = this.userRepo.create(data);
     if (data.roleId) {
-      const role = await this.rolesService.findOne(data.roleId);
+      const role = await this.rolesRepo.findOne({ where: { id: data.roleId } });
+      if (!role) {
+        throw new NotFoundException(`Role #${data.roleId} not found`);
+      }
       newUser.role = role;
     }
     if (data.shiftId !== undefined) {
-      const shift = data.shiftId ? await this.shiftsService.findOne(data.shiftId) : null;
+      const shift = data.shiftId ? await this.shiftsRepo.findOne({ where: { id: data.shiftId } }) : null;
+      if (data.shiftId && !shift) {
+        throw new NotFoundException(`Shift #${data.shiftId} not found`);
+      }
       newUser.shift = shift;
     }
     return this.userRepo.save(newUser);
@@ -50,11 +56,17 @@ export class UsersService {
       throw new NotFoundException(`User #${id} not found`);
     }
     if (changes.roleId) {
-      const role = await this.rolesService.findOne(changes.roleId);
+      const role = await this.rolesRepo.findOne({ where: { id: changes.roleId } });
+      if (!role) {
+        throw new NotFoundException(`Role #${changes.roleId} not found`);
+      }
       user.role = role;
     }
     if (changes.shiftId !== undefined) {
-      const shift = changes.shiftId ? await this.shiftsService.findOne(changes.shiftId) : null;
+      const shift = changes.shiftId ? await this.shiftsRepo.findOne({ where: { id: changes.shiftId } }) : null;
+      if (changes.shiftId && !shift) {
+        throw new NotFoundException(`Shift #${changes.shiftId} not found`);
+      }
       user.shift = shift;
     }
     this.userRepo.merge(user, changes);
